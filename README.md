509186ccceee91250e66679e159ccc42abd3c52c
Svilen Vassilev <svilen@rubystudio.net>
2013-01-06 16:24:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+== vateud.net
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VateudNet::Application.config.secret_token = 'ef3b1399aa1abfaa58d2a7bc8da6278674b8dcb4c8e9790516394f0d2b9ee1f996ebf8aec01d5aedfc3082cc92ea014b54ec6a69b8e2920e49f261615d487ead'
