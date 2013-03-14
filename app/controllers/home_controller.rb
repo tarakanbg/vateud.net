5d24c24f8e6e75ed817fcba85906f8eef0621269
Svilen Vassilev <svilen@rubystudio.net>
2013-03-14 15:47:32 +0200

    admin bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,14 @@ class HomeController < ApplicationController
     @page = Page.where(:slug => "home").first
   end
 
-  def mercury_update
-    if admin_user_signed_in?
-      page =Page.find(params[:id])
-      #page.name = params[:content][:page_name][:value]
-      page.post = params[:content][:page_content][:value]
-      page.save!
-      render text: ""        
-    end
-  end
+  # def mercury_update
+  #   if admin_user_signed_in?
+  #     page =Page.find(params[:id])
+  #     #page.name = params[:content][:page_name][:value]
+  #     page.post = params[:content][:page_content][:value]
+  #     page.save!
+  #     render text: ""        
+  #   end
+  # end
   
 end
