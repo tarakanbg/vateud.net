5f1292b57e243a842dac11c16252ad9253686d93
Svilen Vassilev <svilen@rubystudio.net>
2014-02-11 19:40:45 +0200

    initializers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ VateudNet::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( leaflet.js leaflet.css)
+  config.assets.precompile += %w( leaflet.js leaflet.css mercury.js jquery-1.7.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
