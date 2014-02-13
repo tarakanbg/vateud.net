c67680bf721abd8c2ba0e72acb4d8298414e7e21
Svilen Vassilev <svilen@rubystudio.net>
2014-02-13 04:41:23 +0200

    freqs

--- app/models/country.rb
+++ app/models/country.rb
@@ -3,13 +3,14 @@ class Country < ActiveRecord::Base
   attr_accessor :frequencies
   belongs_to :subdivision, :inverse_of => :countries
   has_many :airports
+  belongs_to :frequency_country, :inverse_of => :country
 
   validates :code, :name, :presence => true
 
   scope :eud, -> { where(eud: true) }
 
   def frequencies
-    self.frequency_countries.first.frequencies
+    self.frequency_country.frequencies
   end
 
   # default_scope order('name DESC')
